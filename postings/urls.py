15c27212fbdb1967a0c386f2ce231a94b2bd5a11
kim-hoontae <zhzhdkzi@naver.com>
2021-10-27 01:42:17 +0900

    ADD : 글 목록 확인

--- postings/urls.py
+++ postings/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from postings.views import PostingView
+from postings.views import PostingView, PostsView
 
 
 urlpatterns = [
     path('', PostingView.as_view()),
     path('/<int:posting_id>', PostingView.as_view()),
+    path('/list', PostsView.as_view())
 ]
--- postings/views.py
+++ postings/views.py
@@ -1,17 +1,19 @@
 import json
 
-from django.http     import JsonResponse
-from django.views    import View
+from django.http           import JsonResponse
+from django.views          import View
+from django.db.models      import Q
+from django.core.paginator import Paginator ,EmptyPage, PageNotAnInteger
 
-from postings.models import Posting
-from users.utils     import login_decorator
+from postings.models       import Posting
+from users.utils           import login_decorator
 
 class PostingView(View):
     @login_decorator
     def post(self,request):
         try:
-            data     = json.loads(request.body)
-            user     = request.user
+            data = json.loads(request.body)
+            user = request.user
 
             if data['text'] =='':
                 return JsonResponse({'message':'WRITE_A_TEXT'})
@@ -20,22 +22,22 @@ class PostingView(View):
                 user = user,
                 text = data['text']
             )
-            return JsonResponse({'message':'SUCCESS'},status=200)
+            return JsonResponse({'message':'SUCCESS'},status=201)
         except KeyError:
             return JsonResponse({'message':'KEY_ERORR'},status=400)
     
     def get(self,request,posting_id):
-        user = request.user
-        postings = Posting.objects.filter(id=posting_id, user=user)
+        postings = Posting.objects.filter(id=posting_id)
 
         if not postings.exists():
             return JsonResponse({'message':'NOT_POSTING'},status=400)
 
         results = [{
             'id'     : posting.id,
+            'user_id': posting.user_id,
+            'name'   : posting.user.name,
             'text'   : posting.text,
             'created': posting.created_at,
-            'updated': posting.updated_at
             }for posting in postings]
 
         return JsonResponse({'results':results},status=200)
@@ -49,7 +51,7 @@ class PostingView(View):
 
         postings = Posting.objects.filter(id=posting_id, user=request.user) 
         if not postings.exists():
-            return JsonResponse({'message':'NOT_POSTING'},status=404)
+            return JsonResponse({'message':'NOT_POSTING'},status=401)
 
         Posting.objects.filter(id=posting_id).update(
             text = data['text']
@@ -60,12 +62,38 @@ class PostingView(View):
     def delete(self,request,posting_id):
 
         if not Posting.objects.filter(id=posting_id, user = request.user).exists():
-            return JsonResponse({'message':'NOT_FOUND'},status=404)
+            return JsonResponse({'message':'INVALID_USER'},status=401)
 
         posting = Posting.objects.get(id=posting_id, user = request.user)
         posting.delete()
 
-        return JsonResponse({'message':'DELETE_SUCCESS'},status=200)
+        return JsonResponse({'message':'DELETE_SUCCESS'},status=204)
 
+class PostsView(View):
+    def get(self,request):
+        page = request.GET.get('page',1)
+        user = request.GET.get('user_id', None)
 
+        filters = Q()
+        if user:
+            filters &= Q(user = user)
+
+        posting_list = Posting.objects.filter(filters).order_by('id')
+        paginator    = Paginator(posting_list,3)
+        try:
+            postings = paginator.page(page)
+        except PageNotAnInteger:
+            postings = paginator.page(1)
+        except EmptyPage:
+            postings = paginator.page(paginator.num_pages)
+
+        results = [{
+            'id'        : posting.id,
+            'user_id'   : posting.user_id,
+            'name'      : posting.user.name,
+            'text'      : posting.text,
+            'created_at': posting.created_at
+        }for posting in postings]
+
+        return JsonResponse({'page':page,'all_post':results},status=200)
         
